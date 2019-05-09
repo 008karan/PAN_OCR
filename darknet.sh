5aa1e599053fdd515f36bd9763c1a89fa0fbaf64
Karan Purohit <008kaaraan@gmail.com>
2019-05-09 06:45:17 +0530

    darknet installation

---
+++ darknet.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#Tested on Ubuntu 16.04.
+
+
+
+
+echo -e "\n\nDownloading darknet\n\n"
+wget https://www.dropbox.com/s/9nxzvyyi53bi4p4/darknet?dl=0
+mv darknet?dl=0 darknet
+chmod 755 darknet
+
+
+#needed darknet. Moved to pwd.
